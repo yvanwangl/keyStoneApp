c059e5704106559c7c323c1f807375adc654b29d
wangyafei <wangyafei@startimes.com.cn>
2016-10-20 22:15:27 +0800

    modify style

--- routes/middleware.js
+++ routes/middleware.js
@@ -19,10 +19,10 @@ var _ = require('lodash');
 */
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
-		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
-		{ label: 'Contact', key: 'contact', href: '/contact' },
+		{ label: '首页', key: 'home', href: '/' },
+		{ label: '博客', key: 'blog', href: '/blog' },
+		{ label: '图集', key: 'gallery', href: '/gallery' },
+		{ label: '联系', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
 	next();
